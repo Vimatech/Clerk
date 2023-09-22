040fd3a0d005d3440469647b052d7a13f9adc679
Mads Steiner <mads.steiner.kristensen@lego.com>
2023-09-22 19:24:01 +0200

    feat: using predefined event types

--- src/Clerk.Webhooks/WebhookEvent.cs
+++ src/Clerk.Webhooks/WebhookEvent.cs
@@ -5,69 +5,75 @@ namespace Clerk.Webhooks;
 /// </summary>
 public class WebhookEvent
 {
-    public class User
-    {
-        public string Created => "user.created";
-        
-        public string Updated => "user.updated";
-        
-        public string Deleted => "user.deleted";
-    }
+    public Email Email { get; } = new();
     
-    public class Email
-    {
-        public string Created => "email.created";
-    }
+    public User User { get; } = new();
+    
+    public Organization Organization { get; } = new();
+    
+    public Session Session { get; } = new();
+    
+    public Sms Sms { get; } = new();
+    
+}
 
-    public class Organization
-    {
-        public string Created => "organization.created";
+public class Email
+{
+    public string Created => "email.created";
+}
+
+public class User
+{
+    public string Created => "user.created";
         
-        public string Updated => "organization.updated";
+    public string Updated => "user.updated";
         
-        public string Deleted => "organization.deleted";
+    public string Deleted => "user.deleted";
+}
 
-        public class Domain
-        {
-            public string Created => "organizationDomain.created";
-            
-            public string Updated => "organizationDomain.updated";
-            
-            public string Deleted => "organizationDomain.deleted";
-        }
-
-        public class Invitation
-        {
-            public string Accepted => "organizationInvitation.accepted";
-            
-            public string Created => "organizationInvitation.created";
-            
-            public string Revoked => "organizationInvitation.revoked";
-        }
-
-        public class Membership
-        {
-            public string Created => "organizationMembership.created";
-            
-            public string Updated => "organizationMembership.updated";
-            
-            public string Deleted => "organizationMembership.deleted";
-        }
-    }
-
-    public class Session
-    {
-        public string Created => "session.created";
+public class Organization
+{
+    public string Created => "organization.created";
         
-        public string Ended => "session.ended";
+    public string Updated => "organization.updated";
         
-        public string Removed => "session.removed";
-        
-        public string Revoked => "session.revoked";
-    }
+    public string Deleted => "organization.deleted";
+    
+    public Invitation Invitation { get; } = new();
+    
+    public Membership Membership { get; } = new();
+}
 
-    public class Sms
-    {
-        public string Created => "sms.created";
-    }
+public class Invitation
+{
+    public string Accepted => "organizationInvitation.accepted";
+            
+    public string Created => "organizationInvitation.created";
+            
+    public string Revoked => "organizationInvitation.revoked";
+}
+
+public class Membership
+{
+    public string Created => "organizationMembership.created";
+            
+    public string Updated => "organizationMembership.updated";
+            
+    public string Deleted => "organizationMembership.deleted";
+}
+
+public class Session
+{
+    public string Created => "session.created";
+        
+    public string Ended => "session.ended";
+        
+    public string Removed => "session.removed";
+        
+    public string Revoked => "session.revoked";
+}
+
+public class Sms
+{
+    public string Created => "sms.created";
 }
